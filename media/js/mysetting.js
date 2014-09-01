4cba874751ca1cb0d6c3840842eff1987c112c4d
angela0 <zuchuang1@gmail.com>
2014-09-01 18:35:22 +0800

    noblank

--- media/js/mysetting.js
+++ media/js/mysetting.js
@@ -2,9 +2,19 @@
 
 function alter_a_tag (tagname) {
 	var a = document.getElementsByTagName(tagname);
+    document.write(a[0].classList[0])
 	for (var i = 0; i < a.length; i++) {
-		if(a[i].classList != 'noblank'){
+		if (findstr(a[i].classList, 'noblank')) {
 			a[i].target = '_blank';
 		}
-	};
+	}
+}
+
+function findstr(list, str) {
+	for(var i = 0; i < list.length; ++i){
+        if(list[i]===str){
+            return true;
+        }
+    }
+	return false;
 }
